3b09b0b45c73602b1c7e0d49713a4dd17651aec1
Boris Ponsioen <boris1592@gmail.com>
2013-12-30 18:19:47 +0100

    Weinig updates

--- Les3/cipher.rb
+++ Les3/cipher.rb
@@ -11,10 +11,11 @@ def encrypt (message, shift)
 	# Neem een bericht en codeer deze met de hash uit de build-hash functie met
 	# een gegeven shift
 
-	
+
 end
 
 def decrypt (encryptedmessage, shift)
 	# Decodeer een bericht weer, als je de shift weet. Hint: dit kan vrij kort!
+
 
 end
